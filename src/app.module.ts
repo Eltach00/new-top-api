9df6548d333fca86d096501a8dcdaf6735a9c505
Eltach00 <eltacquliyev@gmail.com>
2023-04-16 17:42:34 +0400

    mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,19 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
 import { ProductModule } from './product/product.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/test')],
+  imports: [
+    MongooseModule.forRootAsync(getMongoConfig()),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    AuthModule,
+    TopPageModule,
+    ReviewModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
